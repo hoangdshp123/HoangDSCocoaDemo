a66bd836d398408f6b4215eec5dbc24ee661cf58
hoang <hoangphuongalhp96@gmail.com>
2020-01-03 10:55:22 +0700

    show progress

--- HoangDSCocoaDemo/UILoginSSO/LoginSSOVC.swift
+++ HoangDSCocoaDemo/UILoginSSO/LoginSSOVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import KRProgressHUD
 
 protocol loginDelegate {
     func tokenReturn (_ token: String)
@@ -25,8 +26,13 @@ class LoginSSOVC: UIViewController {
 
     
     @IBAction func loginClicked(_ sender: Any) {
-        self.dismiss(animated: true) {}
-        onComplete?( "eyJhbGciOiJFUzI1NiJ9.eyJzdWIiOiIwMzI3NDk3OTk2IiwiU1NPSWQiOiIzNTE3MGIyMi1iZGE5LTRlOGMtYjk0OC0xYzc5MjJhOGJhMDMiLCJVc2VySWQiOjcyMDA3OTcsIkZyb21Tb3VyY2UiOjIsIlRva2VuIjoiMTEzNkJDODVBNTM0OUVCMzcxMDFEOTczOTRBRENBNzciLCJleHAiOjE2MDg4OTAxMzAsIlBhcnRuZXIiOjB9.H36r4_GGiCJbtD4BhEH3sBYEuXe7F-Z8OBvqt3AaFO87QJQ9fhG6J2mOZyV0sojgKHVQeId_VxiAKArARmaTkw")
+        KRProgressHUD.show()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) { // Change `2.0` to the desired number of seconds.
+            KRProgressHUD.dismiss()
+            self.dismiss(animated: true) {}
+            self.onComplete?( "eyJhbGciOiJFUzI1NiJ9.eyJzdWIiOiIwMzI3NDk3OTk2IiwiU1NPSWQiOiIzNTE3MGIyMi1iZGE5LTRlOGMtYjk0OC0xYzc5MjJhOGJhMDMiLCJVc2VySWQiOjcyMDA3OTcsIkZyb21Tb3VyY2UiOjIsIlRva2VuIjoiMTEzNkJDODVBNTM0OUVCMzcxMDFEOTczOTRBRENBNzciLCJleHAiOjE2MDg4OTAxMzAsIlBhcnRuZXIiOjB9.H36r4_GGiCJbtD4BhEH3sBYEuXe7F-Z8OBvqt3AaFO87QJQ9fhG6J2mOZyV0sojgKHVQeId_VxiAKArARmaTkw")
+        }
+        
     }
     
 }
